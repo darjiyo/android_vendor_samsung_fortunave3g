3d5ab4a8c20ece1bb2d91f71df56b2c864960822
DARJIYO <djarjiyocute@gmail.com>
2017-10-07 22:16:05 +0700

    Update fortunave3g-vendor.mk

--- fortunave3g-vendor.mk
+++ fortunave3g-vendor.mk
@@ -1,5 +1,5 @@
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortunave3g/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/lenovo/a6000/overlay
 
 # Include non-open-source files
-$(call inherit-product, vendor/samsung/fortunave3g/fortunave3g-vendor-blobs.mk)
+$(call inherit-product, vendor/lenovo/a6000/a6000-vendor-blobs.mk)
